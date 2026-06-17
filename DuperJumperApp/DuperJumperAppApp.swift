7826ee0291050e99d0d5da3eb494870016b19a62
npoway <n.smoliar2013@gmail.com>
2026-06-17 16:49:25 +0300

    Initial Commit

---
+++ DuperJumperApp/DuperJumperAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DuperJumperAppApp.swift
+//  DuperJumperApp
+//
+//  Created by Никита on 17.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct DuperJumperAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
